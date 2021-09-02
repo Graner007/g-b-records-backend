b0cc86660cff31ad1ed5f93e5dafa8e0be616b9e
Graner007 <graner.bence007@gmail.com>
2021-09-02 10:28:56 +0200

    return grandTotal in cart resolver

--- src/resolvers/cart/carts.ts
+++ src/resolvers/cart/carts.ts
@@ -10,7 +10,7 @@ const resolvers = {
         });
       },
       cart: async (_parent: any, _args: any, context: Context) => {
-        return context.prisma.cart.findUnique({
+        const cart = await context.prisma.cart.findUnique({
           where: {
             userId: context.userId
           },
@@ -21,6 +21,13 @@ const resolvers = {
             throw new Error("Cart not found");
           })
         });
+
+        let grandTotal = 0;
+        if (cart.products && cart.products.length > 0) {
+          cart.products.forEach(product => grandTotal += product.price);
+        }
+
+        return { cart, grandTotal };
       }
     }
 };
