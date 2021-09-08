1fe00a5b8917caa3650e4b9ad2f740dc58cf2e5b
Graner007 <graner.bence007@gmail.com>
2021-09-08 14:10:58 +0200

    add addCheckoutDetail resolver

--- src/resolvers/checkdetails.ts
+++ src/resolvers/checkdetails.ts
@@ -1,10 +1,33 @@
 import { Context } from "../context";
 import { user } from "./user/userUtils";
 
+type AddCheckoutDetailArgs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: string;
+  zipcode: number;
+  country: string;
+  telephone: string;
+}
+
 const resolvers = {
     Mutation: {
-      addCheckoutDetail: async (_parent: any, _args: any, context: Context) => {
+      addCheckoutDetail: async (_parent: any, args: AddCheckoutDetailArgs, context: Context) => {
         const currentUser = await user(context);
+
+        return context.prisma.checkoutDetail.create({
+          data: {
+            firstName: args.firstName,
+            lastName: args.lastName,
+            email: args.email,
+            address: args.address,
+            zipcode: args.zipcode,
+            country: args.country,
+            phone: args.telephone,
+            userId: currentUser.id
+          }
+        });
       }
     }
 };
